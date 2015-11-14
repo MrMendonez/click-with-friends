64fe01f81479485c54c52ff9d549ac5080b75c5f
MrMendonez <dmendonez@gmail.com>
2015-11-14 14:08:50 -0500

    added counter and countdown to js

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+// scripts for click with friends game
+
+function startTimer(){
+  decTimer = setInterval(myTimer, 1000);
+};
+
+var secondsLeft = 20;
+
+var count = 0
+var clickableImages = document.getElementsByClassName("count-click");
+
+function timer() {
+  if(secondsLeft > 0) {
+    document.getElementById("time").innerHTML = secondsLeft-=1;
+  } else {
+      document.getElementById("timeLine").innerHTML = "Your time is up.";
+      document.getElementById("timeLine").className = "change";
+      alert("Congratulations! You click " + count + " characters!");
+      clearInterval(decTimer);
+  }
+}
+
+function counter() {
+  if (this.getAttribute('data-clickable') == 'true') {
+    count++;
+    this.setAttribute('data-clickable', 'false');
+  }
+  document.getElementById("clickTotal").innerHTML = count;
+}
+
+for(i = 0; i < clickableImages.length; i++){
+  documentclickableImages[i].addEventListener("click", counter); 
+}
+
+// from shamoon's demo
+
+// var secondsCount = 0;
+// var timerInterval;
+// var toggleBtn = document.getElementById("toggleBtn");
+
+// setTimeout(function() {
+//   toggleBtn.parentNode.removeChild(toggleBtn);
+//   alert("You need to playfast!");
+// }, 15000);
+
+// function addSeconds() {
+//   secondsCount++;
+// }
+
+// function toggleTime()  {
+//   if(this.getAttribute("data-slate") === "start") {
+//     this.innerHTML = "Stop";
+//     this.setAttribute("data-state", "stop");
+//     secondsCount = 0;
+//     timerInterval = setInterval(addSeconds, 1000);
+//   } else {
+
+//   }
+// }
