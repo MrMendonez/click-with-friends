5d7f0314d3025908ee90a9836608fffc8f016c99
MrMendonez <dmendonez@gmail.com>
2015-11-15 16:29:54 -0500

    got clicks to start counting once timer starts and added an alert as soon as player wins game

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,22 @@
 // scripts for Click With Friends game
 
 var decTimer;
-var secondsLeft = 3;
+var secondsLeft = 20;
 var count = 0;
 var clickableImages = document.getElementsByClassName("count-click");
 
 // TIMER FUNCTIONS
 
-// Start countdown by clicking on start button:
-document.getElementById("startButton").addEventListener("click", startTimer);
-
 // Sets countdown to decrease by 1 every second
 function startTimer(){
   decTimer = setInterval(timer, 1000);
-  this.setAttribute('data-clickable', 'true');
+  for (var i = 0; i < clickableImages.length; i++) {
+    clickableImages[i].setAttribute("data-clickable", "true");
+  }
 };
+
+// Anonymous Function/Event Listener - Start countdown by clicking on start button:
+document.getElementById("startButton").addEventListener("click", startTimer);
 
 // Shows the countdown on the page. When countdown ends it initiates game over sequence
 function timer() {
@@ -29,9 +31,7 @@ function timer() {
 // Game over sequence
 function gameOver() {
   alert("Your time is up!");
-  if (count === 20) {
-    alert("You clicked all " + count + " characters! You won! Congratulations!")
-  } else if (count === 0) {
+  if (count === 0) {
     alert("You didn't click any charcters. You needed to click on all 20 characters. You lose!")
   } else if (count === 1) {
     alert("You clicked " + count + " character. You needed to click on 19 more. You lose!")
@@ -49,6 +49,10 @@ function counter() {
     this.setAttribute('data-clickable', 'false');
   }
   document.getElementById("clickTotal").innerHTML = count;
+  if (count === 20) {
+    alert("You clicked all " + count + " characters with " + secondsLeft + " seconds left to spare! You won! Congratulations! =)")
+    clearInterval(decTimer);
+  }
 };
 
 for(i = 0; i < clickableImages.length; i++) {
