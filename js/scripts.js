c9cbf067a1d093f5034792ffe94afdd6c45c7dd9
MrMendonez <dmendonez@gmail.com>
2015-11-15 21:44:20 -0500

    added comments to counter functions

--- js/scripts.js
+++ js/scripts.js
@@ -45,8 +45,10 @@ function gameOver() {
 
 // CLICK COUNTER FUNCTIONS
 
+// Increase click count when player clicks on each character and
+// engages win message if player wins.
 function counter() {
-  if (this.getAttribute('data-clickable') == 'true') {
+  if (this.getAttribute('data-clickable') === 'true') {
     count++;
     this.setAttribute('data-clickable', 'false');
   }
@@ -59,6 +61,8 @@ function counter() {
   }
 };
 
+// Anonymous Function/Event Listener - when player clicks on character 
+// counter function is engaged.
 for(i = 0; i < clickableImages.length; i++) {
   clickableImages[i].addEventListener("click", counter); 
 };
