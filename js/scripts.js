858804dc9b4162b60111ecc8334ff2230ef883c1
alemjc <alemjc2204@gmail.com>
2016-01-10 23:45:18 -0500

    fixed issues in the site.

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,106 @@
+/*jslint browser:true */
+/*global window*/
 // Scripts for Click With Friends game
 
-//GLOBAL VARIABLES
-
-var decTimer;
-var secondsLeft = 20;
-var count = 0;
-var clickableImages = document.getElementsByClassName("count-click");
-var startButton = document.getElementById("startButton");
+$(document).ready(function(){
+  //GLOBAL VARIABLES
+  "use strict";
+  var decTimer;
+  var secondsLeft = 20;
+  var count = 0;
+  var clickableImages = document.getElementsByClassName("count-click");
+  var startButton = document.getElementById("startButton");
+  $('#verify').on('click',function(){
+    $('#myMessages').text("Jean Carlos Henriquez verified this page");
+    $('#myModal').modal({
+      backdrop: 'static',
+      show: true
+    });
+  })
 
 // TIMER FUNCTIONS
 
-// Sets countdown to decrease by 1 every second
-function startTimer(){
-  decTimer = setInterval(timer, 1000);
-  for (var i = 0; i < clickableImages.length; i++) {
-    clickableImages[i].setAttribute("data-clickable", "true");
-    startButton.setAttribute("class", "btn btn-danger btn-block center-block");
+  // Game over sequence
+  function gameOver() {
+    clearInterval(decTimer);
+
+    if (count === 0) {
+      $('#myMessages').empty();
+      $('#myMessages').text("Your time is up! You didn't click any characters. You needed to click on all 20 characters. You lose!");
+      $('#myModal').modal({
+        backdrop: 'static',
+        show: true
+      }).on('hidden.bs.modal',function(){location.reload();});
+    } else if (count === 1) {
+      $('#myMessages').empty();
+      $('#myMessages').text("Your time is up! You clicked " + count + " character. You need 19 more. You lose!");
+      $('#myModal').modal({
+        backdrop: 'static',
+        show: true
+      }).on('hidden.bs.modal',function(){location.reload();});
+    } else {
+      $('#myMessages').empty();
+      $('#myMessages').text("Your time is up! You clicked on " + count + " characters. You need " + (20 - count) + " more. You lose!");
+      $('#myModal').modal({
+        backdrop: 'static',
+        show: true
+      }).on('hidden.bs.modal',function(){location.reload();});
+    }
+
   }
-};
+
+  // Shows the countdown on the page. When countdown ends it initiates game over sequence
+  function timer() {
+    if(secondsLeft > 0) {
+      secondsLeft = secondsLeft - 1;
+      document.getElementById("time").innerHTML = secondsLeft;
+    }else {
+      gameOver();
+    }
+  }
+
+// Sets countdown to decrease by 1 every second
+  function startTimer() {
+    var i;
+    decTimer = setInterval(timer, 1000);
+    for(i = 0; i < clickableImages.length; i+=1) {
+      clickableImages[i].setAttribute("data-clickable", "true");
+      startButton.setAttribute("class", "btn btn-danger btn-block center-block");
+    }
+  }
 
 // Anonymous Function/Event Listener - Start countdown by clicking on start button:
-startButton.addEventListener("click", startTimer);
-
-// Shows the countdown on the page. When countdown ends it initiates game over sequence
-function timer() {
-  if(secondsLeft > 0) {
-    document.getElementById("time").innerHTML = secondsLeft-=1;
-  } else {
-      gameOver();
-      location.reload();
-  }
-};
-
-// Game over sequence
-function gameOver() {
-  alert("Your time is up!");
-  if (count === 0) {
-    alert("You didn't click any characters. You needed to click on all 20 characters. You lose!")
-  } else if (count === 1) {
-    alert("You clicked " + count + " character. You need 19 more. You lose!")
-  } else {
-    alert("You clicked on " + count + " characters. You need " + (20 - count) + " more. You lose!")
-  }
-  clearInterval(decTimer);
-};
+  startButton.addEventListener("click", startTimer);
 
 // CLICK COUNTER FUNCTIONS
 
-// Anonymous Function/Event Listener - when player clicks on character 
-// counter function is engaged.
-for (i = 0; i < clickableImages.length; i++) {
-  clickableImages[i].addEventListener("click", counter); 
-};
+  // Increase click count when player clicks on each character and
+  // engages win message
+  // if player wins.
+  function counter() {
+    if (this.getAttribute('data-clickable') === 'true') {
+      count = count + 1;
+      this.setAttribute('data-clickable', 'false');
+      this.className = "img-circle count-click img-responsive center-block dimmed";
+    }
+    document.getElementById("clickTotal").innerHTML = count;
+    if (count === 20) {
+      clearInterval(decTimer);
+      $('#myMessages').empty();
+      $('#myMessages').text("You clicked all " + count + " characters in " + (20 - secondsLeft) + " seconds! You won! Congratulations! =) " +
+                            "Now, can you do it faster?");
+      $('#myModal').modal({
+        backdrop: 'static',
+        show: true
+      }).on('hidden.bs.modal',function(){location.reload();});
+    }
+  }
 
-// Increase click count when player clicks on each character and
-// engages win message if player wins.
-function counter() {
-  if (this.getAttribute('data-clickable') === 'true') {
-    count++;
-    this.setAttribute('data-clickable', 'false');
-    this.className = "img-circle count-click img-responsive center-block dimmed";
+// Anonymous Function/Event Listener - when player clicks on character
+// counter function is engaged.
+  var i;
+  for(i = 0; i < clickableImages.length; i+=1) {
+    clickableImages[i].addEventListener("click", counter);
   }
-  document.getElementById("clickTotal").innerHTML = count;
-  if (count === 20) {
-    alert("You clicked all " + count + " characters in " + (20 - secondsLeft) + " seconds! You won! Congratulations! =)")
-    alert("Now, can you do it faster?")
-    clearInterval(decTimer);
-    location.reload();
-  }
-};
+
+});
